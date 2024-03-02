5d684f12fcba9df894f35d18d47e54861a4996ec
Ghanashyam Aryal <anjanaryal13@gmail.com>
2024-03-02 21:45:55 +0545

    redus

---
+++ reduxtodo/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Addtodo from "./component/addtodo"
+import Todolist from './component/Todolist';
+
+
+function App() {
+  return (
+  <>
+  <Addtodo/>
+  <Todolist/>
+  </>
+    
+  )
+}
+
+export default App;
